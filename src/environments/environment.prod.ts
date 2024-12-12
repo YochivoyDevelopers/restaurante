bc4fc03db4d663f009e441f2344dbe11d2c7229a
AlejandroMuGon2001 <alexgzz314@gmail.com>
2024-12-12 13:39:12 -0600

    main

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,31 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDiukyca-NMpzoxy5hd68inYATrm18wlPQ",
+    authDomain: "deliveryapp-54574.firebaseapp.com",
+    databaseURL: "https://deliveryapp-54574-default-rtdb.firebaseio.com",
+    projectId: "deliveryapp-54574",
+    storageBucket: "deliveryapp-54574.appspot.com",
+    messagingSenderId: "239509151869",
+    appId: "1:239509151869:web:c9dd19ccf686ea20ead114",
+    measurementId: "G-RCCQB4JT47",
+  },
+  onesignal: {
+    appId: "473af2db-5227-471e-9f6d-cf4583f798e4",
+    googleProjectNumber: "540145033481",
+    restKey: "NzZkMGQ0MmUtNmE0Ny00YzBmLTliMjgtNTFmZWQzMWYzNzMw"
+  },
+  stripe: {
+    publicKey: "pk_live_51HZSxILl147XFj5b5yREuw1R8KOwczzHwSLEfQ4VaiDibrYFiCuARrEFx0q1EVeRTkOtQ7smxsBLxX83zTJUZ3EU00I6mhAFk3",
+    secretKey: "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"
+  },
+  paypal: {
+    sandbox: "",
+    production:
+      "AUQL8ZowKIFJ0F1BpAqgNR5vh3HUW3YYwnMMUXH3C8LLXpx4tZ9j6tyItEcbGpThF6lnMbPOzAf4WDWp"
+  },
+  general: {
+    symbol: "$",
+    code: "MXN"
+  }
+};
